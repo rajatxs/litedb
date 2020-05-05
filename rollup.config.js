21cf165e2dd4f201e1e33aa9c23538079ea1ce7d
rajatprototype <rajat.prototype@gmail.com>
2020-05-05 13:37:59 +0530

    Revert "Update rollup config, support umd, cjs and es module format"
    This reverts commit ccb995662458c9c105fd476a4fe1d3e910fb5d7e.

--- rollup.config.js
+++ rollup.config.js
@@ -2,26 +2,9 @@
 export default {
   input: "./lib/index.js",
 
-  output: [
-    {
-      format: 'iife',
-      name: "LiteDB",
-      file: "./dist/litedb.js"
-    },
-    {
-      format: 'umd',
-      name: "LiteDB",
-      file: "./dist/litedb.umd.js"
-    },
-    {
-      format: 'cjs',
-      name: "LiteDB",
-      file: "./dist/litedb.cjs.js"
-    },
-    {
-      format: 'es',
-      name: "LiteDB",
-      file: "./dist/litedb.mjs"
-    }
-  ]
+  output: {
+    format: 'umd',
+    name: "LiteDB",
+    file: "./dist/litedb.js"
+  }
 }
