72ca74ffed5a1fca387f1e2e4130d4f7cc419820
rajatprototype <rajat.prototype@gmail.com>
2020-05-06 16:14:57 +0530

    define EventEmitter

--- src/utils.ts
+++ src/utils.ts
@@ -8,3 +8,31 @@ export const getEntries = (pattern: string): Array<string> => {
   const keylist = Object.getOwnPropertyNames(localStorage) // All keys
   return keylist.filter(key => key.includes(pattern))
 }
+
+/**
+ * Event handlers
+ * @class
+ * @extends {EventTarget}
+ */
+export abstract class EventEmitter extends EventTarget {
+  /**
+   * Dispatch an event
+   * @param {string} evname - Event name
+   * @param {EventInit} options - Event initial options
+   * @returns {boolean}
+   */
+  public emit(evname: string, options: EventInit): boolean {
+    return this.dispatchEvent(new Event(evname, options))
+  }
+
+  /**
+   * Set event listener
+   * @param {string} evname - Event name
+   * @param {EventListenerOrEventListenerObject} handler Event handler callback
+   * @param {boolean | AddEventListenerOptions} options - Event options
+   * @returns {void}
+   */
+  public on(evname: string, handler: EventListenerOrEventListenerObject, options: boolean | AddEventListenerOptions): void {
+    return this.addEventListener(evname, handler, options)
+  }
+}
