7a63fb6514733dba085ada5f005f907842a5ccc7
rajatprototype <rajat.prototype@gmail.com>
2020-06-14 16:48:27 +0530

    Update local system

--- lib/index.d.ts
+++
@@ -1,5 +0,0 @@
-import LiteDB from './litedb';
-/**
- * @exports LiteDB
- */
-export default LiteDB;
--- rollup.config.js
+++ rollup.config.js
@@ -1,27 +1,36 @@
+import typescript from 'rollup-plugin-typescript2'
+import pkg from './package.json'
+import { terser } from 'rollup-plugin-terser'
 
 export default {
-  input: "./lib/index.js",
+  input: './src/index.ts',
 
   output: [
     {
+      file: './dist/litedb.js',
       format: 'iife',
-      name: "LiteDB",
-      file: "./dist/litedb.js"
+      name: "LiteDB"
     },
     {
+      file: './dist/litedb.umd.js',
       format: 'umd',
-      name: "LiteDB",
-      file: "./dist/litedb.umd.js"
+      name: 'LiteDB'
     },
     {
-      format: 'cjs',
-      name: "LiteDB",
-      file: "./dist/litedb.cjs.js"
-    },
-    {
-      format: 'es',
-      name: "LiteDB",
-      file: "./dist/litedb.mjs"
+      file: './dist/litedb.mjs',
+      format: 'es'
     }
+  ],
+  external: [
+    ...Object.keys(pkg.dependencies || {})
+  ],
+  plugins: [
+    typescript({
+      typescript: require('typescript')
+    }),
+    terser({
+      keep_classnames: true,
+      keep_fnames: true
+    }) // minifies generated bundles
   ]
 }
